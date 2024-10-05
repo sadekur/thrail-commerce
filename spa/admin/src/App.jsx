8ac5c4e4796dde0aaf5de05531a912d516b7f6fe
Sadekur Rahman <shadekur.rahman60@gmail.com>
2024-10-05 21:36:04 +0600

    misc

--- spa/admin/src/App.jsx
+++ spa/admin/src/App.jsx
@@ -13,14 +13,12 @@ const App = () => {
 export default App;
 
 // ReactDOM.createRoot(document.getElementById("thrail_commerce_render")).render(
-// // 	<App />
-// // );
+// 	<App />
+// );
 
-const rootElement = document.getElementById('thrail_commerce_render');
+const rootElement = document.getElementById("thrail_commerce_render");
 
 if (rootElement) {
-  const root = ReactDOM.createRoot(rootElement);  // This line can throw the error if `rootElement` is null
-  root.render(<App />);
-} else {
-  console.error("Target container 'root' is not found.");
+	const root = ReactDOM.createRoot(rootElement);
+	root.render(<App />);
 }
