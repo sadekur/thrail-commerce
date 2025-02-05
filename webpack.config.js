192d8f8c22f4546e09e27d9ed87ff27d4356477a
Sadekur Rahman <shadekur.rahman60@gmail.com>
2025-02-05 22:30:55 +0600

    block some structure change for build file

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = (env, argv) => {
 		mode: argv.mode || "development",
 		entry: {
 			admin: path.resolve(__dirname, "spa/admin/src/App.jsx"),
-			block: path.resolve(__dirname, "spa/blocks/App.jsx"),
+			block: path.resolve(__dirname, "blocks/App.jsx"),
 			public: path.resolve(__dirname, "spa/public/src/App.jsx"),
 			tailwind: path.resolve(__dirname, "assets/css/tailwind.css"),
 		},
