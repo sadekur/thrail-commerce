7bf51cd965e3b45b44bd85828a67021096ad3db2
Sadekur Rahman <shadekur.rahman60@gmail.com>
2025-02-09 21:43:19 +0600

    block direction change

--- app/Blocks.php
+++ app/Blocks.php
@@ -13,35 +13,35 @@ class Blocks {
             'product' => __( 'Thrail - Commerce', 'thrail-commerce' ),
         ];
 
-        $this->filter('init', [ $this, 'blocks_register' ]);
-        $this->filter('block_categories_all', [ $this, 'add_custom_categories' ]);
+        $this->filter( 'init', [ $this, 'blocks_register' ] );
+        $this->filter( 'block_categories_all', [ $this, 'add_custom_categories' ] );
     }
 
     public function blocks_register() {
-        $blocks_dir     = THRAIL_COMMERCE_PATH . 'spa/blocks/';
-        $categories     = glob($blocks_dir); // Fetch directories
-        $block_settings = get_option('thrail_commerce_block_settings');
-        $block_settings = maybe_unserialize($block_settings);
+        $blocks_dir     = THRAIL_COMMERCE_PATH . 'blocks/';
+        $categories     = glob( $blocks_dir );
+        $block_settings = get_option( 'thrail_commerce_block_settings' );
+        $block_settings = maybe_unserialize( $block_settings );
 
         $active_blocks = [];
 
-        foreach ($categories as $category) {
-            $category_name = basename($category);
-            $blocks = glob($category . '/*', GLOB_ONLYDIR);
+        foreach ( $categories as $category ) {
+            $category_name = basename( $category );
+            $blocks        = glob( $category . '/*', GLOB_ONLYDIR );
 
-            foreach ($blocks as $block) {
-                $block_name = basename($block);
+            foreach ( $blocks as $block ) {
+                $block_name       = basename( $block );
                 $block_option_key = "{$block_name}";
 
-                if (isset($block_settings[$block_option_key]) && $block_settings[$block_option_key] === 'on') {
+                if ( isset( $block_settings[$block_option_key] ) && $block_settings[$block_option_key] === 'on' ) {
                     $active_blocks[] = $block_name;
-                    register_block_type($block);
+                    register_block_type( $block );
                 }
             }
         }
 
         // Pass the list of active blocks to JavaScript
-        wp_localize_script('thrail-commerce-block-script', 'activeBlocks', $active_blocks);
+        wp_localize_script( 'thrail-commerce-block-script', 'activeBlocks', $active_blocks );
     }
 
     /**
