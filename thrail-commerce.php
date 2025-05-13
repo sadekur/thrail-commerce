48855a9c520f9369ce826b5bc56a0fb488e48e30
Sadekur Rahman <shadekur.rahman60@gmail.com>
2025-05-13 22:14:28 +0600

    requare plugin add

--- thrail-commerce.php
+++ thrail-commerce.php
@@ -11,6 +11,7 @@
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:        thrail-commerce
+ * Require Plugin:     woocommerce
  * Domain Path:       /languages
  *
  */
