8e5419bc05c7f9f4ff315a40b16c377d242561bd
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-02-01 23:27:41 +0600

    Update: stockthreshold class

---
+++ features/stock-threshold-for-wc/stock-threshold-for-wc.php
@@ -0,0 +1,10 @@
+<?php
+namespace Thrail\Commerce\Features;
+use Thrail\Commerce\Classes\Trait\Hookable;
+class StockThresholdForWc {
+    use Hookable;
+
+    public function __construct() {
+        // Register hooks and shortcodes.
+    }
+}
