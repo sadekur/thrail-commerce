430c596fbd6dfd77ce0ea175f671c5c78a124806
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-04-18 23:35:29 +0600

    Add:  saperate component for svgs

--- spa/admin/src/pages/StockThreshold.jsx
+++ spa/admin/src/pages/StockThreshold.jsx
@@ -5,6 +5,7 @@ import SettingSkeleton from "../../common/Skeletons/SettingSkalaton";
 import FieldRow from "../../common/FieldRow";
 import NumberField from "../../common/NumberField";
 import CheckboxField from "../../common/CheckboxField";
+import { WarningIcon } from "../../common/Svgs";
 
 /* ─────────────────────────────────────────────
    Thin section divider
@@ -138,9 +139,7 @@ const StockThreshold = () => {
                 <div className="mt-6 p-8 bg-yellow-50 border-l-4 border-yellow-400 rounded-lg">
                     <div className="flex">
                         <div className="flex-shrink-0">
-                            <svg className="h-8 w-8 text-yellow-400" viewBox="0 0 20 20" fill="currentColor">
-                                <path fillRule="evenodd" d="M8.257 3.099c.765-1.36 2.722-1.36 3.486 0l5.58 9.92c.75 1.334-.213 2.98-1.742 2.98H4.42c-1.53 0-2.493-1.646-1.743-2.98l5.58-9.92zM11 13a1 1 0 11-2 0 1 1 0 012 0zm-1-8a1 1 0 00-1 1v3a1 1 0 002 0V6a1 1 0 00-1-1z" clipRule="evenodd" />
-                            </svg>
+                           {WarningIcon}
                         </div>
                         <div className="ml-4">
                             <h3 className="text-lg font-medium text-yellow-800">Feature Not Enabled</h3>
