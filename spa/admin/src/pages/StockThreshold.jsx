7e434aefeec2decb8aaa725ea667b3edca0a9dac
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-04-18 19:53:40 +0600

    Adjust: skalaton component saperate and reusable

--- spa/admin/src/pages/StockThreshold.jsx
+++ spa/admin/src/pages/StockThreshold.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import CommonHeader from "../../common/CommonHeader";
+import SettingSkeleton from "../../common/Skeletons/SettingSkalaton";
 
 const StockThreshold = () => {
     const [isLoading, setIsLoading] = useState(true);
@@ -94,14 +95,7 @@ const StockThreshold = () => {
 
     if (isLoading) {
         return (
-            <div className="relative">
-                {/* <CommonHeader title="Stock Threshold Settings" /> */}
-                <div className="mt-4 p-6 bg-white shadow-md rounded-lg border border-gray-200 animate-pulse">
-                    <div className="h-6 bg-gray-200 rounded w-1/3 mb-4"></div>
-                    <div className="h-10 bg-gray-100 rounded w-full mb-3"></div>
-                    <div className="h-4 bg-gray-100 rounded w-2/3"></div>
-                </div>
-            </div>
+            <SettingSkeleton />
         );
     }
 
