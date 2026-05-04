e079699845f489b1deff5bd573020da158c067c5
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-05-04 22:08:49 +0600

    Update includes/functions.php with CommerceKit constants and references

--- includes/functions.php
+++ includes/functions.php
@@ -4,15 +4,15 @@ if ( !defined('ABSPATH' ) ) {
 	exit;
 }
 /**
- * Function to set up the database table for Thrail CRM
+ * Function to set up the database table for CommerceKit
  */
-// if( ! function_exists( 'thrail_crm_activate' ) ) :
-// 	function thrail_crm_activate() {
+// if( ! function_exists( 'commerce_kit_activate' ) ) :
+// 	function commerce_kit_activate() {
 // 		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 // 		global $wpdb;
-// 		$table_name = $wpdb->prefix . 'thrail_crm_leads';
+// 		$table_name = $wpdb->prefix . 'commerce_kit_leads';
 // 		$charset_collate = $wpdb->get_charset_collate();
-
+//
 // 		$sql = "CREATE TABLE $table_name (
 // 			id mediumint(9) NOT NULL AUTO_INCREMENT,
 // 			name tinytext NOT NULL,
@@ -20,7 +20,7 @@ if ( !defined('ABSPATH' ) ) {
 // 			time datetime DEFAULT CURRENT_TIMESTAMP NOT NULL,
 // 			PRIMARY KEY  (id)
 // 		) $charset_collate;";
-
+//
 // 		dbDelta($sql);
 // 	}
 // endif;
@@ -33,9 +33,9 @@ if ( !defined('ABSPATH' ) ) {
      */
 	if( ! function_exists( 'get_active_blocks' ) ) :
     function get_active_blocks() {
-        $blocks_dir     = THRAIL_COMMERCE_PATH . 'blocks/';
+        $blocks_dir     = COMMERCE_KIT_PATH . 'blocks/';
         $categories     = glob( $blocks_dir );
-        $block_settings = get_option('thrail_commerce_block_settings');
+        $block_settings = get_option('commerce_kit_block_settings');
         $block_settings = maybe_unserialize($block_settings);
 
         $active_blocks = [];
