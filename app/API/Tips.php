c5b9a1917c90a0838206096d177768ca1d3dd412
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-05-12 23:13:41 +0600

    Saperate: Tips  related Api functions

--- app/API/Tips.php
+++ app/API/Tips.php
@@ -9,4 +9,18 @@ class Tips {
     public function tips_permission() {
         return true;
     }
+
+    public function commerce_kit_save_tips( $request ) {
+
+        $tips_settings      = $request->get_json_params() ?? [];
+        $current_settings   = get_option( 'commercekit-tips-settings', [] );
+        $updated_settings   = array_merge( $current_settings, $tips_settings );
+        update_option( 'commercekit-tips-settings', $updated_settings );
+        return rest_ensure_response( 'success' );
+    }
+
+    public function commerce_kit_get_tips() {
+        $tips_settings = get_option( 'commercekit-tips-settings', [] );
+        return rest_ensure_response( $tips_settings );
+    }
 }
