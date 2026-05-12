d22a281c66bb3f8803fcc898da67bff98fe8f788
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-05-12 23:01:40 +0600

    Update: Coading Structure

--- app/settings/settings.php
+++ app/settings/settings.php
@@ -47,12 +47,12 @@
                                 <input type="checkbox" class="sr-only peer" name="tcwt_note" value="on" <?php checked( $tcwt_note, 'on' ); ?>>
                                 <div class="w-11 h-6 bg-gray-200 rounded-full peer-focus:ring-4 peer-focus:ring-blue-300 peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
                             </label>
-                            <p class="text-sm text-gray-500 mt-1"><?php esc_html_e('Enable to display donation note.', 'commerce-kit'); ?></p>
+                            <p class="text-sm text-gray-500 mt-1"><?php esc_html_e( 'Enable to display donation note.', 'commerce-kit' ); ?></p>
                         </td>
                     </tr>
 
                     <tr class="border-b">
-                        <th class="py-4 px-2 font-medium"><?php esc_html_e('Button Color', 'commerce-kit'); ?></th>
+                        <th class="py-4 px-2 font-medium"><?php esc_html_e( 'Button Color', 'commerce-kit' ); ?></th>
                         <td class="py-4 px-2">
                             <input type="color" name="tcwt_btncolor" class="w-10 h-10 p-1 rounded border border-gray-300 cursor-pointer" value=<?php echo esc_attr($tcwt_btncolor); ?>>
                             <p class="text-sm text-gray-500 mt-1"><?php esc_html_e('Select donation button color.', 'commerce-kit'); ?></p>
