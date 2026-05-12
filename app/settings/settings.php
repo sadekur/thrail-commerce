1cffb6869ed0497b23bdea23dba788d987761198
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-05-12 23:01:32 +0600

    Update: Coading Structure

--- app/settings/settings.php
+++ app/settings/settings.php
@@ -44,7 +44,7 @@
                         <th class="py-4 px-2 font-medium"><?php esc_html_e( 'Donation Note', 'commerce-kit' ); ?></th>
                         <td class="py-4 px-2">
                             <label class="relative inline-flex items-center cursor-pointer">
-                                <input type="checkbox" class="sr-only peer" name="tcwt_note" value="on" <?php checked($tcwt_note, 'on'); ?>>
+                                <input type="checkbox" class="sr-only peer" name="tcwt_note" value="on" <?php checked( $tcwt_note, 'on' ); ?>>
                                 <div class="w-11 h-6 bg-gray-200 rounded-full peer-focus:ring-4 peer-focus:ring-blue-300 peer-checked:bg-blue-600 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all"></div>
                             </label>
                             <p class="text-sm text-gray-500 mt-1"><?php esc_html_e('Enable to display donation note.', 'commerce-kit'); ?></p>
