f6c009e45e931b9f837ee5a0d6f1a42cfceca9fb
Sadekur Rahman <shadekur.rahman60@gmail.com>
2026-05-15 23:48:25 +0600

    Remove: Unnecessery commit

--- assets/js/enquiry.js
+++ assets/js/enquiry.js
@@ -1,21 +1,3 @@
 ;(function($) {
 
-    // $('#wedevs-enquiry-form form').on('submit', function(e) {
-    //     e.preventDefault();
-
-    //     var data = $(this).serialize();
-
-    //     $.post(COMMERCEKIT.ajaxurl, data, function(response) {
-    //         if (response.success) {
-    //             console.log(response.success);
-    //         } else {
-    //             alert(response.data.message);
-    //         }
-    //     })
-    //     .fail(function() {
-    //         alert(COMMERCEKIT.error);
-    //     })
-
-    // });
-
 })(jQuery);
