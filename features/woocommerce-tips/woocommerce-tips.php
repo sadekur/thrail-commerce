9f84484cd5fb7db2cfc8eaa8feb48e22bd2bb62a
Sadekur <shadekur.rahman60@gmail.com>
2026-05-17 22:34:27 +0600

    Update: coading structure

--- features/woocommerce-tips/woocommerce-tips.php
+++ features/woocommerce-tips/woocommerce-tips.php
@@ -7,7 +7,7 @@ class WoocommerceTips {
     use Hookable;
     public function __construct() {
         // Hook form display methods
-        $this->action('woocommerce_before_cart_contents', [$this, 'display_tips_form']);
+        $this->action( 'woocommerce_before_cart_contents', [$this, 'display_tips_form'] );
         $this->action('woocommerce_review_order_before_payment', [$this, 'display_tips_form']);
     }
 
