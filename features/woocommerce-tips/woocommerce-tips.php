6603de63f502898d385f7f1ddbd662e88fad15dd
Sadekur <shadekur.rahman60@gmail.com>
2026-05-17 22:35:00 +0600

    Update: coading structure

--- features/woocommerce-tips/woocommerce-tips.php
+++ features/woocommerce-tips/woocommerce-tips.php
@@ -19,7 +19,7 @@ class WoocommerceTips {
     public function get_settings(): array {
         return [
             'tcwt_cart' => get_option( 'tcwt_cart', 'off' ),
-            'tcwt_checkout' => get_option('tcwt_checkout', 'off'),
+            'tcwt_checkout' => get_option( 'tcwt_checkout', 'off' ),
             'tcwt_btncolor' => get_option('tcwt_btncolor', '#b12d0b'),
             'tcwt_btntext' => get_option('tcwt_btntext', 'Submit Tip'),
             'tcwt_textcolor' => get_option('tcwt_textcolor', '#0300d1'),
